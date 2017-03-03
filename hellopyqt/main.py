3c5b7a22717f0bb1ad49e29dc1154c0610a5770a
Eutrema <eutremasoysauce@gmail.com>
2017-03-03 17:12:02 +0900

    インデントにタブ文字とスペースが混在していたので修正

--- hellopyqt/main.py
+++ hellopyqt/main.py
@@ -5,16 +5,16 @@ import test1
 import test2
 
 class mainWindow(QtGui.QMainWindow, mainWindow.Ui_MainWindow):
-	def __init__(self):
-		super(self.__class__, self).__init__()
-		self.setupUi(self)
-		
+  def __init__(self):
+    super(self.__class__, self).__init__()
+    self.setupUi(self)
+    
 class test1Widget(QtGui.QWidget, test1.Ui_Form):
   def __init__(self):
     super(self.__class__, self).__init__()
     self.setupUi(self)
     self.pushButton.clicked.connect(lambda: change(self, test2Widget))
-		
+    
 class test2Widget(QtGui.QWidget, test2.Ui_Form):
   def __init__(self):
     super(self.__class__, self).__init__()
@@ -22,15 +22,15 @@ class test2Widget(QtGui.QWidget, test2.Ui_Form):
     self.pushButton.clicked.connect(lambda: change(self, test1Widget))
 
 def change(widget, next):
-	widget.parentWidget().setCentralWidget(next())
+  widget.parentWidget().setCentralWidget(next())
   
 def main():
-	app = QtGui.QApplication(sys.argv)
-	window = mainWindow()
-	window.setCentralWidget(test1Widget())
-	window.show()
+  app = QtGui.QApplication(sys.argv)
+  window = mainWindow()
+  window.setCentralWidget(test1Widget())
+  window.show()
 
-	app.exec_()
+  app.exec_()
 
 if __name__ == "__main__":
-	main()
+  main()
